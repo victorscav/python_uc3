05e97713abc617cec6f4fce3b6c6fe5e9bc69f69
Victor Neves <vic_scav@hotmail.com>
2025-05-21 20:43:40 -0300

    aula 01

---
+++ aafds.py
@@ -0,0 +1,10 @@
+numero = int(input("Digite um número"))
+
+if numero > 0:
+    print(f"{numero} é positivo.")
+
+if numero < 0:
+    print(f"{numero} é negativo.")
+
+else:
+    print(f("{numero} é 0"))
